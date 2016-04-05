27144e44c690ab42924987994cc12d2f4bd60781
Nathan Hinish <nathan@triadinteractive.com>
2016-04-05 16:49:32 -0700

    adding new test

--- test/index.js
+++ test/index.js
@@ -1,8 +1,14 @@
 'use strict'
 
+const CEC = require('node-cec').CEC
 const CECClient = require('../lib/CECClient')
 const client = new CECClient('cec-tester')
 
 client.start()
+
 client.getCecVersion()
 client.reportPowerStatus()
+
+// F0:64:00:48:65:6C:6C:6F:20:77:6F:72:6C:64
+client.setOsdString(CEC.LogicalAddress.TV, 'Hello world'.split())
+
