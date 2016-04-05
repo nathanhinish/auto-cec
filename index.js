f4c5c2d69c2a10dcacc5ec9b4f22d256e5085695
Nathan Hinish <nathan@triadinteractive.com>
2016-04-05 10:35:29 -0700

    adding code

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const { NodeCec, CEC } = require( 'node-cec' )
+
+const cec = new NodeCec('monitor')
+
+cec.once( 'ready', ( client ) ->
+  console.log(' -- READY -- ')
+  client.sendCommand( 0xf0, CEC.Opcode.GIVE_DEVICE_POWER_STATUS )
+)
+
+// events from: http://www.cec-o-matic.com/
+cec.on( 'REPORT_POWER_STATUS', (packet, status) => {
+  for {power_status_name, power_status} of CEC.PowerStatus) {
+    if (power_status === status)
+      console.log(`POWER_STATUS: ${power_status_name}`)
+      break
+    }
+})
+
+cec.on( 'ROUTING_CHANGE', (packet, fromSource, toSource) => {
+  console.log(`Routing changed from ${fromSource} to ${toSource}.`)
+)
+
+cec.start( 'cec-client', '-m', '-d', '8', '-b', 'r' )
