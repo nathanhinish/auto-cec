5e4d3bc0e683c7c2b706ec1a13248fc5893d74ca
Nathan Hinish <nathan@triadinteractive.com>
2016-04-05 16:53:54 -0700

    adding new test

--- test/index.js
+++ test/index.js
@@ -9,7 +9,7 @@ client.getCecVersion()
 client.reportPowerStatus()
 
 // F0:64:00:48:65:6C:6C:6F:20:77:6F:72:6C:64
-let chars = 'Hello world'.split().map((c) => {
+let chars = 'Hello world'.split('').map((c) => {
   return c.charCodeAt(0)
 })
 console.info(chars.map((c) => {
