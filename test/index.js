13ad14a938cd1d2c7423931cf94276d509626ea4
Nathan Hinish <nathan@triadinteractive.com>
2016-04-05 21:10:40 -0700

    trying to get test to work

--- test/index.js
+++ test/index.js
@@ -1,36 +1,17 @@
 'use strict'
 
-const keys = require('lodash/keys')
-const commandTests = require('require-dir')('./commands')
-const handlerTests = require('require-dir')('./handlers')
+const path = require('path')
+const glob = require('glob')
 
-const CECClient = require('../lib/CECClient')
+const commandTestSuites = glob.sync('commands/setOsdString.js', {cwd: __dirname})
+const handlerTestSuites = glob.sync('handlers/setOsdString.js', {cwd: __dirname})
 
-const commandKeys = keys(commandTests)
-const handlerKeys = keys(handlerTests)
-
-const AFTER_TIMEOUT = 60000
-
-let client
+// const AFTER_TIMEOUT = 60000
 
 describe('CECClient', () => {
-  before((done) => {
-    client = new CECClient('cec-tester')
-    client.start(done)
-    console.info('before', client)
-  })
-
-  after((done) => {
-    client.stop(done)
-    client = null
-    console.info('after', client)
-  })
-
   describe('Commands', function () {
-    console.info('test', client)
-    describe('Commands#setOsdString', function () {
-      console.info('test 2', client)
-      commandTests.setOsdString(client)
+    commandTestSuites.forEach((file) => {
+      require(`./${file}`)
     })
   })
 
