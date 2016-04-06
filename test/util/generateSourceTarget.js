03c4e10b378ae7ddbe6df616883136f27f775f60
Nathan Hinish <nathan@triadinteractive.com>
2016-04-05 23:46:47 -0700

    testing

--- test/util/generateSourceTarget.js
+++ test/util/generateSourceTarget.js
@@ -6,7 +6,7 @@ const RECORDINGDEVICE1 = 1
 const AUDIOSYSTEM = 5
 const OUTPUT = 0x15
 
-describe.only('reduceArgsBytes', function () {
+describe('generateSourceTarget', function () {
   const generateSourceTarget = require('../../lib/util/generateSourceTarget')
   it('should convert inputs to the correct value', function () {
     const value = generateSourceTarget(RECORDINGDEVICE1, AUDIOSYSTEM)
