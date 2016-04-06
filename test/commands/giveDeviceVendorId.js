5f76a30a48c92650f5f8fcf96ae052b460860317
Nathan Hinish <nathan@triadinteractive.com>
2016-04-05 21:48:49 -0700

    trying to get test to work

--- test/commands/giveDeviceVendorId.js
+++ test/commands/giveDeviceVendorId.js
@@ -5,7 +5,10 @@ const sinon = require('sinon')
 
 const ClientProxy = require('../common/ClientProxy')
 
-const Commands = require('../../lib/CECClient').Commands
+const CECClient = require('../../lib/CECClient')
+const Commands = CECClient.Commands
+const Opcode = CECClient.Opcode
+const InverseOpcode = CECClient.InverseOpcode
 
 const proxy = new ClientProxy()
 
@@ -23,10 +26,18 @@ describe.only('#giveDeviceVendorId', function() {
     expect(Commands.giveDeviceVendorId).toExist('Commands.giveDeviceVendorId is not defined')
   })
 
-  it('should get response', function(done) {
+  it('should get response', function (done) {
     this.timeout(6000)
     proxy.target.on('packet', function (packet) {
       console.info('PACKET', JSON.stringify(packet))
+      setTimeout(() => {
+        expect(1).toBe(1)
+        done()
+      }, 5000)
+    })
+
+    proxy.target.on(InverseOpcode.DEVICE_VENDOR_ID, function (packet) {
+      console.info(InverseOpcode.DEVICE_VENDOR_ID, JSON.stringify(packet))
       setTimeout(() => {
         expect(1).toBe(1)
         done()
